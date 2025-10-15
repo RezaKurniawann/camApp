ecaa2154d9a34d74206b5c56c71b0450479f7c1b
rezakurniawann <reza.kurniawan0110@gmail.com>
2025-10-15 13:26:46 +0700

    add apply filter

--- resources/views/partials/filters.blade.php
+++ resources/views/partials/filters.blade.php
@@ -1,5 +1,4 @@
 {{-- resources/views/partials/filters.blade.php --}}
-{{-- Sidebar Filter - Hanya tampil di laptop/desktop (>= 1024px) --}}
 <aside class="w-70 flex-shrink-0 self-start" style="position: sticky; top: 80px; display: none;">
     <style>
         @media (min-width: 1024px) {
@@ -53,13 +52,14 @@
             </div>
 
             <!-- Filter Content with Scroll -->
-            <div class="overflow-y-auto" style="max-height: calc(100vh - 180px);">
+            <div class="overflow-y-auto" style="max-height: calc(100vh - 260px);">
                 @include('partials.filter-item', [
                     'id' => 'serverFilter',
                     'title' => 'Server Name',
                     'icon' => 'M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2',
                     'name' => 'servers',
-                    'items' => $servers
+                    'items' => $servers,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -67,7 +67,8 @@
                     'title' => 'Brand',
                     'icon' => 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
                     'name' => 'brands',
-                    'items' => $brands
+                    'items' => $brands,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -75,7 +76,8 @@
                     'title' => 'Variant',
                     'icon' => 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z',
                     'name' => 'variants',
-                    'items' => $variants
+                    'items' => $variants,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -83,7 +85,8 @@
                     'title' => 'Type',
                     'icon' => 'M7 20l4-16m2 16l4-16M6 9h14M4 15h14',
                     'name' => 'types',
-                    'items' => $types
+                    'items' => $types,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -91,7 +94,8 @@
                     'title' => 'Region',
                     'icon' => 'M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
                     'name' => 'regions',
-                    'items' => $regions
+                    'items' => $regions,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -99,7 +103,8 @@
                     'title' => 'Company',
                     'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4',
                     'name' => 'companies',
-                    'items' => $companies
+                    'items' => $companies,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -107,7 +112,8 @@
                     'title' => 'Location',
                     'icon' => 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z',
                     'name' => 'locations',
-                    'items' => $locations
+                    'items' => $locations,
+                    'isMobile' => false
                 ])
 
                 @include('partials.filter-item', [
@@ -116,8 +122,17 @@
                     'icon' => 'M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z',
                     'name' => 'categories',
                     'items' => $categories,
+                    'isMobile' => false,
                     'isLast' => true
                 ])
+            </div>
+
+            <!-- Apply Filter Button (Sticky at bottom) -->
+            <div class="border-t border-gray-200 p-4 bg-white">
+                <button type="submit" 
+                    class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2.5 rounded-lg transition-colors shadow-sm hover:shadow-md">
+                    Apply Filters
+                </button>
             </div>
         </div>
     </form>
